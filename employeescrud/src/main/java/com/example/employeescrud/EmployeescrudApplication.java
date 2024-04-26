edfa775e6530ce1ad66b9b7d584e069bd9d8b6e4
jaafari-R <jaafariramadan@gmail.com>
2024-04-26 12:24:18 +0300

    Added Employee Entity and DB Configurations

---
+++ employeescrud/src/main/java/com/example/employeescrud/EmployeescrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeescrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeescrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeescrudApplication.class, args);
+	}
+
+}
