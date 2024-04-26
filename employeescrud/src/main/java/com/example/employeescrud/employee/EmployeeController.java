885327129e0f054bbf8ea8af2a4e02ddce260a4e
jaafari-R <jaafariramadan@gmail.com>
2024-04-26 16:15:19 +0300

    Implemented Get EmployeeById, Create, Update, and Delete Employee Controller methods

--- employeescrud/src/main/java/com/example/employeescrud/employee/EmployeeController.java
+++ employeescrud/src/main/java/com/example/employeescrud/employee/EmployeeController.java
@@ -3,8 +3,14 @@ package com.example.employeescrud.employee;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +26,26 @@ public class EmployeeController {
     @GetMapping("/")
     public List<Employee> getAllEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/{employeeId}")
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        return employeeService.findById(employeeId);
+    }
+
+    @PostMapping("/")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    // @ResponseStatus() // 204
+    @DeleteMapping("/{employeeId}")
+    public void deleteEmployee(@PathVariable int employeeId) {
+        employeeService.delete(employeeId);
     }
 }
